4b98eafd13dff3f006117bbc886158e9bbeab3b2
palomapaine1 <palomapaine4@gmail.com>
2024-11-28 21:55:46 -0300

    Update untitled7.py

--- untitled7.py
+++ untitled7.py
@@ -1,8 +1,6 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import seaborn as sns
-from io import BytesIO
+import requests
 
 # Cargar los datos procesados
 @st.cache
